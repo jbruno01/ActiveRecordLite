eab9ccdbe888e476e48b8a554f1042ab2946484d
John Bruno <jbruno01@gmail.com>
2015-06-12 16:43:16 -0400

    Write searchable module. Mixin to SQLobjects.

--- lib/02_searchable.rb
+++ lib/02_searchable.rb
@@ -1,12 +1,26 @@
 require_relative 'db_connection'
 require_relative '01_sql_object'
-
+require 'byebug'
 module Searchable
   def where(params)
-    # ...
+    where_line = params.map { |key,val| "#{self.table_name}.#{key.to_s} = ?"}
+    vals = params.map {|key, val| params[key]}
+    where_line = where_line.join(" AND ")
+
+    # byebug
+    thing = DBConnection.execute(<<-SQL, *vals)
+      SELECT
+        *
+      FROM
+        #{self.table_name}
+      WHERE
+        #{where_line}
+    SQL
+
+    parse_all(thing)
   end
 end
 
 class SQLObject
-  # Mixin Searchable here...
+  extend Searchable
 end
