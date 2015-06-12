9d98f52253c232618b9ab96b94867739336a7eaa
John Bruno <jbruno01@gmail.com>
2015-06-12 11:24:50 -0400

    my_attr_accessor object.

--- lib/00_attr_accessor_object.rb
+++ lib/00_attr_accessor_object.rb
@@ -1,5 +1,32 @@
 class AttrAccessorObject
   def self.my_attr_accessor(*names)
-    # ...
+
+    # setter
+    names.each do |name|
+      define_method("#{name}=") do |arg|
+        instance_variable_set("@#{name}", arg)
+      end
+    end
+
+    # getter
+    names.each do |name|
+      define_method(name) do
+        instance_variable_get("@#{name}")
+      end
+    end
+
+  end
+end
+
+
+class Cat
+  attr_accessor :color
+
+  def color
+    @color
+  end
+
+  def color=(arg)
+    @color = arg
   end
 end
