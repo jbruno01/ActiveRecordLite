7bd02b3aabf74a70effb7c1a3770ede935beb1db
John Bruno <jbruno01@gmail.com>
2015-06-24 22:18:50 -0400

    Finish project. All specs pass.

--- lib/03_associatable.rb
+++ lib/03_associatable.rb
@@ -11,46 +11,74 @@ class AssocOptions
   )
 
   def model_class
-    # ...
+    @class_name.constantize
   end
 
   def table_name
-    # ...
+    model_class.table_name
   end
 end
 
 class BelongsToOptions < AssocOptions
   def initialize(name, options = {})
-    byebug
-    if options == {}
-      @foreign_key = "#{name}_id".to_sym
-      @primary_id = :id
-      @class_name = "#{name.camelcase}"
+    default = {
+      :foreign_key => "#{name}_id".to_sym,
+      :primary_key => :id,
+      :class_name => name.to_s.camelcase
+    }
+
+    default.keys.each do |key|
+      self.send("#{key}=", options[key] || default[key])
     end
   end
 end
 
 class HasManyOptions < AssocOptions
   def initialize(name, self_class_name, options = {})
-    # ...
+    default = {
+      :foreign_key => "#{self_class_name.underscore}_id".to_sym,
+      :primary_key => :id,
+      :class_name => name.to_s.singularize.camelcase
+    }
+
+    default.keys.each do |key|
+      self.send("#{key}=", options[key] || default[key])
+    end
   end
 end
 
 module Associatable
   # Phase IIIb
   def belongs_to(name, options = {})
-    # ...
+    self.assoc_options[name] = BelongsToOptions.new(name, options)
+
+    define_method(name) do
+      options = self.class.assoc_options[name]
+
+      key_value = self.send(options.foreign_key)
+      options.model_class.where(options.primary_key => key_value).first
+
+    end
   end
 
   def has_many(name, options = {})
-    # ...
+    self.assoc_options[name] = HasManyOptions.new(name, self.name, options)
+
+    define_method(name) do
+      options = self.class.assoc_options[name]
+
+      key_value = self.send(options.primary_key)
+      options.model_class.where(options.foreign_key => key_value)
+
+    end
   end
 
   def assoc_options
-    # Wait to implement this in Phase IVa. Modify `belongs_to`, too.
+    @assoc_options ||= {}
+    @assoc_options
   end
 end
 
 class SQLObject
-  # Mixin Associatable here...
+  extend Associatable
 end
